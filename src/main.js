77384b1448fa3220c7cd54fd4048a94f89ad55a3
giovsteph <giov.ponce@gmail.com>
2019-11-14 11:22:41 -0600

    button start hover

--- src/main.js
+++ src/main.js
@@ -2,11 +2,3 @@ import RICKANDMORTY from './data/rickandmorty/rickandmorty.js'
 import { example } from './data.js';
 console.log(example);
 console.log(RICKANDMORTY);
-
-
-document.getElementById("strtBtn").addEventListener("click", nextPage);
-
-const nextPage = () => {
-    document.getElementById("startPage").style.display = "none";
-    document.getElementById("characterPage").style.display = "block";
-}
