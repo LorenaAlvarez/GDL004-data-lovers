2fa1c5f8ce8d35e6e78bef747d6aa7dca22ac9c1
giovsteph <giov.ponce@gmail.com>
2019-11-20 08:20:34 -0600

    change Lore in CSS

--- src/data.js
+++ src/data.js
@@ -1,25 +1,23 @@
 import RICKANDMORTY from './data/rickandmorty/rickandmorty.js'
 console.log(RICKANDMORTY);
+
 let i = 1;
 for (let personaje of RICKANDMORTY.results) {
-    console.log(personaje.name);
-    console.log(personaje.species);
-    console.log(personaje.episode.map(v => {
+    let personajeName = personaje.name;
+    console.log(personajeName);
+    let personajeEspecie = personaje.species;
+    console.log(personajeEspecie);
+    let personajeEpisode = personaje.episode.map(v => {
         if (v.length === 41) {
             return v.slice(-1)
         } else { return v.slice(-2) }
-    }));
+    });
+    console.log(personajeEpisode);
+
     if (i === 15) {
         break; // Se rompe el for
     }
-
     i++;
-}
 
-let personaje = RICKANDMORTY.results;
-const filterByNameFunction = (name) => {
-    return name === Rick;
-}
 
-let filtrados = personaje.name.filter(filterByNameFunction);
-console.log(filtrados);
+}
