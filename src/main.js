698a0351d1183982954955f46190fb38e434fa53
giovsteph <giov.ponce@gmail.com>
2019-11-26 21:32:45 -0600

    boton ir arriba de Lore

--- src/main.js
+++ src/main.js
@@ -26,6 +26,26 @@ import {
 
 //FUNCIONALIDADES DE MOSTRAR OCULTAR
 
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+document.getElementById("myBtn").addEventListener("click", topFunction)
+
+
 //BUTTON START / hide start and show characters
 document.getElementById("container").addEventListener("click", function() {
     document.getElementById("characterPage").style.display = "block"
